0e0f696ae98b21dde3bf4ad6b2d274738f8da57b
Georgi Popov <magadanski_uchen@yahoo.com>
2016-12-28 19:51:05 +0200

    test functionality

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 'use strict';
 
 const express = require('express');
+const https = require('https');
+const bodyParser = require('body-parser');
+const crypto = require('crypto');
+const config = require('config');
 
 var app = express();
+app.set('port', process.env.PORT || 5000);
+app.use(bodyParser.json({ verify: verifyRequestSignature }));
+app.use(express.static('public'));
+
+const APP_SECRET = (process.env.NOTIFIER_APP_SECRET) ? process.env.NOTIFIER_APP_SECRET : config.get('appSecret');
+const VALIDATION_TOKEN = (process.env.NOTIFIER_VALIDATION_TOKEN) ? (process.env.NOTIFIER_VALIDATION_TOKEN) : config.get('validationToken');
+const PAGE_ACCESS_TOKEN = (process.env.NOTIFIER_PAGE_ACCESS_TOKEN) ? (process.env.NOTIFIER_PAGE_ACCESS_TOKEN) : config.get('pageAccessToken');
+const SERVER_URL = (process.env.SERVER_URL) ? (process.env.SERVER_URL) : config.get('serverURL');
+
+if (!(APP_SECRET && VALIDATION_TOKEN && PAGE_ACCESS_TOKEN && SERVER_URL)) {
+	console.error("Missing config values");
+	process.exit(1);
+}
 
 app.get('/', function (req, res) {
-	if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === '^sdB]~"i4+1v 3b') {
+	if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === VALIDATION_TOKEN) {
 		console.log("Validating webhook");
 		res.status(200).send(req.query['hub.challenge']);
 	} else {
@@ -13,3 +30,29 @@ app.get('/', function (req, res) {
 		res.sendStatus(403);
 	}
 });
+
+function verifyRequestSignature(req, res, buf) {
+	var signature = req.headers["x-hub-signature"];
+	
+	if (!signature) {
+		console.error("Couldn't validate the signature.");
+	} else {
+		var elements = signature.split('=');
+		var method = elements[0];
+		var signatureHash = elements[1];
+		
+		var expectedHash = crypto.createHmac('sha1', APP_SECRET)
+			.update(buf)
+			.digest('hex');
+		
+		if (signatureHash != expectedHash) {
+			throw new Error("Couldn't validate the request signature.");
+		}
+	}
+}
+
+app.listen(app.get('port'), function () {
+	console.log('Node app is running on port', app.get('port'));
+});
+
+module.exports = app;
