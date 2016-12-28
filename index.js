d95f98adaebcf1290cc139292c3ca2c510c18373
Georgi Popov <magadanski_uchen@yahoo.com>
2016-12-28 22:19:05 +0200

    switch crypto for xhub (heroku friendly)

--- index.js
+++ index.js
@@ -3,12 +3,15 @@
 const express = require('express');
 const https = require('https');
 const bodyParser = require('body-parser');
-const crypto = require('crypto');
+// const crypto = require('crypto');
+const xhub = require('express-x-hub');
 const config = require('config');
 
 var app = express();
-app.set('port', process.env.PORT || 5000);
-app.use(bodyParser.json({ verify: verifyRequestSignature }));
+app.set('port', (process.env.PORT || 5000));
+app.listen(app.get('port'));
+app.use(xhub({ algorithm: 'sha1', secret: process.env.APP_SECRET }));
+app.use(bodyParser.json());
 app.use(express.static('public'));
 
 const APP_SECRET = (process.env.NOTIFIER_APP_SECRET) ? process.env.NOTIFIER_APP_SECRET : config.get('appSecret');
@@ -51,23 +54,20 @@ app.post('/webhook', function (req, res) {
 });
 
 function verifyRequestSignature(req, res, buf) {
-	var signature = req.headers["x-hub-signature"];
-	
-	if (!signature) {
-		console.error("Couldn't validate the signature.");
-	} else {
-		var elements = signature.split('=');
-		var method = elements[0];
-		var signatureHash = elements[1];
+	if (req.isXHub()) {
+		console.log('request header X-Hub-Signature found, validating');
 		
-		var expectedHash = crypto.createHmac(method, APP_SECRET)
-			.update(buf)
-			.digest('hex');
-		
-		if (signatureHash != expectedHash) {
-			throw new Error("Couldn't validate the request signature.\nRequested signature: " + signature + "\nSHA1: " + crypto.createHmac('sha1', APP_SECRET) + "\nBuf: " + buf + "\nExpected signature: " + expectedHash);
+		if (req.isXHubValid()) {
+			console.log('request header X-Hub-Signature validated');
+			res.send('Verified!\n');
 		}
+	} else {
+		console.log('Warning - request header X-Hub-Signature not present or invalid');
+		res.send('Failed to verify!\n');
 	}
+	
+	console.log(req.body);
+	// res.sendStatus(200);
 }
 
 function receivedMessage(event) {
