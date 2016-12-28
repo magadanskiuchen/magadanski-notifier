434e8afd3ff0c177687658fa9ae6baa71d6956fa
Georgi Popov <magadanski_uchen@yahoo.com>
2016-12-28 20:13:08 +0200

    sample FB code to send messages

--- index.js
+++ index.js
@@ -31,6 +31,25 @@ app.get('/', function (req, res) {
 	}
 });
 
+app.post('/webhook', function (req, res) {
+	if (req.body.object === 'page') {
+		req.body.entry.forEach(function (entry) {
+			var pageID = entry.id;
+			var timeOfEvent = entry.time;
+			
+			entry.messaging.forEach(function (event) {
+				if (event.message) {
+					receivedMessage(event);
+				} else {
+					console.log("Webhook received unknown event: ", event);
+				}
+			});
+		});
+		
+		res.sendStatus(200);
+  }
+});
+
 function verifyRequestSignature(req, res, buf) {
 	var signature = req.headers["x-hub-signature"];
 	
@@ -49,6 +68,70 @@ function verifyRequestSignature(req, res, buf) {
 			throw new Error("Couldn't validate the request signature.");
 		}
 	}
+}
+
+function receivedMessage(event) {
+	var senderID = event.sender.id;
+	var recipientID = event.recipient.id;
+	var timeOfMessage = event.timestamp;
+	var message = event.message;
+	
+	console.log("Received message for user %d and page %d at %d with message:", senderID, recipientID, timeOfMessage);
+	console.log(JSON.stringify(message));
+	
+	var messageId = message.mid;
+	var messageText = message.text;
+	var messageAttachments = message.attachments;
+	
+	if (messageText) {
+		switch (messageText) {
+			case 'generic':
+			sendGenericMessage(senderID);
+			break;
+		
+		default:
+			sendTextMessage(senderID, messageText);
+		}
+	} else if (messageAttachments) {
+		sendTextMessage(senderID, "Message with attachment received");
+	}
+}
+
+function sendGenericMessage(recipientId) {
+	sendTextMessage(recipientId, 'This is a response to a message of "generic"');
+}
+
+function sendTextMessage(recipientId, messageText) {
+	var messageData = {
+		recipient: {
+			id: recipientId
+		},
+		message: {
+			text: messageText
+		}
+	};
+	
+	callSendAPI(messageData);
+}
+
+function callSendAPI(messageData) {
+	request({
+		uri: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: { access_token: PAGE_ACCESS_TOKEN },
+		method: 'POST',
+		json: messageData
+	}, function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			var recipientId = body.recipient_id;
+			var messageId = body.message_id;
+			
+			console.log("Successfully sent generic message with id %s to recipient %s", messageId, recipientId);
+		} else {
+			console.error("Unable to send message.");
+			console.error(response);
+			console.error(error);
+		}
+	});
 }
 
 app.listen(app.get('port'), function () {
